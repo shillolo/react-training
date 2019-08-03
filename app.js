f1c4d324400216b9c68105a0f3302bdf7b439473
Darren <darren.igb@web.de>
2019-08-04 00:22:54 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,139 +1,145 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var expressHbs = require('express-handlebars');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
-var session = require('express-session');
-var passport = require('passport');
-var flash = require('connect-flash');
-var validator = require('express-validator');
-var MongoStore = require('connect-mongo')(session);
-var schedule = require('node-schedule');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var expressHbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require("passport");
+var flash = require("connect-flash");
+var validator = require("express-validator");
+var MongoStore = require("connect-mongo")(session);
+var schedule = require("node-schedule");
 
-var Bun = require('./models/buns');
-var Credit = require('./models/credit')
-var User_Order = require('./models/user-order');
-var Order = require('./models/order')
-var indexRouter = require('./routes/index');
-var userRoutes = require('./routes/user');
+var Bun = require("./models/buns");
+var Bakery = require("./models/bakerys");
+var Credit = require("./models/credit");
+var User_Order = require("./models/user-order");
+var Order = require("./models/order");
+var indexRouter = require("./routes/index");
+var userRoutes = require("./routes/user");
 
 var app = express();
-var http = require('http').Server(app);
+var http = require("http").Server(app);
 
 var server = app.listen(3000);
-var io = require('socket.io').listen(server);
-
+var io = require("socket.io").listen(server);
 
 // update stores (on new order) per websocket
-io.on('connection', function(socket){
-  User_Order.watch().
-    on('change', function(){
-      // datenbanken vergleichen per input auf html
-      User_Order.find(function(err, order) {
-        var fullArray = [];
-        var counter = 0;
-        if (err) {
-        } else {
-            for(var i = 0; i < order.length; i++){
-                var thisday = new Date(new Date().setDate(new Date().getDate() + 1)).setHours(0,0,0,0)
-                var newday = new Date(order[i].date).setHours(0,0,0,0);
-                var currentDay = new Date().setHours(0,0,0,0)
-                if(thisday == newday || newday == currentDay){
-                    counter++
-                    var thisOrder = ({
-                        position: counter,
-                        order: order[i]
-                    })
-                    fullArray.push(thisOrder)
-                } 
-            }
+io.on("connection", function(socket) {
+  User_Order.watch().on("change", function() {
+    // datenbanken vergleichen per input auf html
+    User_Order.find(function(err, order) {
+      var fullArray = [];
+      var counter = 0;
+      if (err) {
+      } else {
+        for (var i = 0; i < order.length; i++) {
+          var thisday = new Date(
+            new Date().setDate(new Date().getDate() + 1)
+          ).setHours(0, 0, 0, 0);
+          var newday = new Date(order[i].date).setHours(0, 0, 0, 0);
+          var currentDay = new Date().setHours(0, 0, 0, 0);
+          if (thisday == newday || newday == currentDay) {
+            counter++;
+            var thisOrder = {
+              position: counter,
+              order: order[i]
+            };
+            fullArray.push(thisOrder);
+          }
         }
-        Order.find(function(err, norder) {
-            if (err) {
-                console.log("error")
-            } else {
-                for(var i = 0; i < norder.length; i++){
-                    var thisday = new Date(new Date().setDate(new Date().getDate() + 1)).setHours(0,0,0,0)
-                    var newday = new Date(norder[i].date).setHours(0,0,0,0);
-                    var currentDay = new Date().setHours(0,0,0,0)
-                    if(newday == thisday || newday == currentDay){
-                        counter++
-                    var thisOrder = ({
-                        position: counter,
-                        order: norder[i]
-                    })
-                    fullArray.push(thisOrder)
-                    } 
-                }
+      }
+      Order.find(function(err, norder) {
+        if (err) {
+          console.log("error");
+        } else {
+          for (var i = 0; i < norder.length; i++) {
+            var thisday = new Date(
+              new Date().setDate(new Date().getDate() + 1)
+            ).setHours(0, 0, 0, 0);
+            var newday = new Date(norder[i].date).setHours(0, 0, 0, 0);
+            var currentDay = new Date().setHours(0, 0, 0, 0);
+            if (newday == thisday || newday == currentDay) {
+              counter++;
+              var thisOrder = {
+                position: counter,
+                order: norder[i]
+              };
+              fullArray.push(thisOrder);
             }
-            console.log("ttttt")
-            console.log(fullArray)
-            console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
-            console.log(fullArray.length)
-            // signal gets commited so that the javascript file can catch the signal via websocket
-            io.emit('chatmessage', fullArray.length);
-        });
-      })
-    })
+          }
+        }
+        console.log("ttttt");
+        console.log(fullArray);
+        console.log(
+          "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+        );
+        console.log(fullArray.length);
+        // signal gets commited so that the javascript file can catch the signal via websocket
+        io.emit("chatmessage", fullArray.length);
+      });
+    });
+  });
 });
 
-require('./config/passport');
-
+require("./config/passport");
 
 // reset blosed bakerys and soldout buns to open and available
-schedule.scheduleJob('0 0 * * *', function(){
-    Bun.find(function(err, buns) {
-      for (var i = 0; i < buns.length; i++){
-        buns[i].clicked = false;
-        buns[i].expdate = false;
-        buns.save()
-      }
-    })
-    Bakery.find(function(err, status){
-      status[0].closed = false;
-      status.save()
-    })
-    console.log("didi it")
+schedule.scheduleJob("28 0 * * *", function() {
+  Bun.find(function(err, buns) {
+    for (var i = 0; i < buns.length; i++) {
+      buns[i].clicked = false;
+      buns[i].expdate = false;
+      buns[i].save();
+    }
+  });
+  Bakery.find(function(err, status) {
+    status[0].closed = true;
+    status[0].save();
+  });
+  console.log("didi it");
 });
 
 // view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}))
-app.set('view engine', 'hbs');
+app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-    secret: 'k7ig8fdjhrrtnfgtcftr0e',
+app.use(
+  session({
+    secret: "k7ig8fdjhrrtnfgtcftr0e",
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection}),
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
     cookie: { maxAge: 180 * 60 * 1000 }
-}));
+  })
+);
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function(req, res, next) {
-    res.locals.login = req.isAuthenticated();
-    res.locals.session = req.session;
-    if (req.user) {
+  res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
+  if (req.user) {
     res.locals.useracc = req.user.email;
     res.locals.knumber = req.user.knumber;
-    }
-    next()
-})
+  }
+  next();
+});
 
-app.use('/user', userRoutes); 
-app.use('/', indexRouter);
+app.use("/user", userRoutes);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -144,16 +150,16 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only provisding error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
 module.exports = app;
--- seed/bakery-seeder.js
+++ seed/bakery-seeder.js
@@ -1,7 +1,10 @@
-var mongoose = require('mongoose');
-var Bakery = require('../models/bakerys.js')
+var mongoose = require("mongoose");
+var Bakery = require("../models/bakerys.js");
 
-mongoose.connect('mongodb+srv://brotritter:brotritter@cluster0-odbbr.mongodb.net/shopping', { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb+srv://brotritter:brotritter@cluster0-odbbr.mongodb.net/shopping",
+  { useNewUrlParser: true }
+);
 
 mongoose.Promise = global.Promise;
 
@@ -9,122 +12,125 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-var bakerys = [,
-    new Bakery({
-    imagePath: '../images/cropped-Bäckermeister-Haferkamp-3-gestaucht.jpg',
-    title: 'Bäckermeister Haferkamp',
+var bakerys = [
+  ,
+  new Bakery({
+    imagePath: "../images/cropped-Bäckermeister-Haferkamp-3-gestaucht.jpg",
+    title: "Bäckermeister Haferkamp",
     address: "H-H-Meier Alee 33",
     closed: false,
     storelink: "http://localhost:3000/bakery",
     buns: [
-        {
-                imagePath: '../images/weltmeister.jpg',
-                title: 'Weltmeister',
-                price: 0.67,
-                ofprice: "0,67",
-                ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                expdate: "",
-                clicked: false
-        },
-        {
-                imagePath: '../images/weltmeister.jpg',
-                title: 'Croissant',
-                price: 0.82,
-                ofprice: "0,82",
-                ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                expdate: "",
-                clicked: false
-        },
-        {
-                imagePath: '../images/weltmeister.jpg',
-                title: 'Normales',
-                price: 0.32,
-                ofprice: "0,32",
-                ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                expdate: "",
-                clicked: false
-        },
-        {
-                imagePath: '../images/weltmeister.jpg',
-                title: 'Weltmeister',
-                price: 0.67,
-                ofprice: "0,67",
-                ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                expdate: "",
-                clicked: false
-        },
-        {
-                imagePath: '../images/weltmeister.jpg',
-                title: 'Normales',
-                price: 0.32,
-                ofprice: "0,32",
-                ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                expdate: "",
-                clicked: false
-        }]
-    }),
-    new Bakery({
-        coordinates: 
-        imagePath: '../images/cropped-Bäckermeister-Haferkamp-3-gestaucht.jpg',
-        title: 'Bäckermeister Haferkamp',
-        address: "Krogstraße 33",
-        closed: false,
-        storelink: "http://localhost:3000/bakery",
-        buns: [
-            {
-                    imagePath: '../images/weltmeister.jpg',
-                    title: 'Weltmeister',
-                    price: 0.67,
-                    ofprice: "0,67",
-                    ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                    expdate: "",
-                    clicked: false
-            },
-            {
-                    imagePath: '../images/weltmeister.jpg',
-                    title: 'Croissant',
-                    price: 0.82,
-                    ofprice: "0,82",
-                    ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                    expdate: "",
-                    clicked: false
-            },
-            {
-                    imagePath: '../images/weltmeister.jpg',
-                    title: 'Normales',
-                    price: 0.32,
-                    ofprice: "0,32",
-                    ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                    expdate: "",
-                    clicked: false
-            },
-            {
-                    imagePath: '../images/weltmeister.jpg',
-                    title: 'Weltmeister',
-                    price: 0.67,
-                    ofprice: "0,67",
-                    ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                    expdate: "",
-                    clicked: false
-            },
-            {
-                    imagePath: '../images/weltmeister.jpg',
-                    title: 'Normales',
-                    price: 0.32,
-                    ofprice: "0,32",
-                    ingredients: ["Weizenmehl", "Wasser", "Salz"],
-                    expdate: "",
-                    clicked: false
-            }]
-        }),
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Weltmeister",
+        price: 0.67,
+        ofprice: "0,67",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      },
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Croissant",
+        price: 0.82,
+        ofprice: "0,82",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      },
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Normales",
+        price: 0.32,
+        ofprice: "0,32",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      },
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Weltmeister",
+        price: 0.67,
+        ofprice: "0,67",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      },
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Normales",
+        price: 0.32,
+        ofprice: "0,32",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      }
+    ]
+  }),
+  new Bakery({
+    coordinates: 33,
+    imagePath: "../images/cropped-Bäckermeister-Haferkamp-3-gestaucht.jpg",
+    title: "Bäckermeister Haferkamp",
+    address: "Krogstraße 33",
+    closed: false,
+    storelink: "http://localhost:3000/bakery",
+    buns: [
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Weltmeister",
+        price: 0.67,
+        ofprice: "0,67",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      },
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Croissant",
+        price: 0.82,
+        ofprice: "0,82",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      },
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Normales",
+        price: 0.32,
+        ofprice: "0,32",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      },
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Weltmeister",
+        price: 0.67,
+        ofprice: "0,67",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      },
+      {
+        imagePath: "../images/weltmeister.jpg",
+        title: "Normales",
+        price: 0.32,
+        ofprice: "0,32",
+        ingredients: ["Weizenmehl", "Wasser", "Salz"],
+        expdate: "",
+        clicked: false
+      }
+    ]
+  })
 ];
 
 for (var i = 1; i < bakerys.length; i++) {
-   bakerys[i].save(function(err, result) {
-        if (err) return handleError(err);
-})
+  bakerys[i].save(function(err, result) {
+    if (err) return handleError(err);
+  });
 }
 
-console.log(bakerys)
+console.log(bakerys);
